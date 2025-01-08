6d63da72cebc8af9f4aab4488f4ad3c58ae1191d
ukaushik-98 <kaushikuk101@gmail.com>
2025-01-07 23:26:24 -0800

    add comand struct skeletons and enums

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,2 +1,25 @@
 #[derive(Debug)]
-pub enum Commands {}
+pub enum Commands {
+    PING(Ping),
+    GET(Get),
+    SET(Set),
+    PUBLISH(Publish),
+    SUBSCRIBE(Subscribe),
+}
+
+#[derive(Debug)]
+struct Ping {
+    message: String,
+}
+
+#[derive(Debug)]
+struct Set {}
+
+#[derive(Debug)]
+struct Get {}
+
+#[derive(Debug)]
+struct Publish {}
+
+#[derive(Debug)]
+struct Subscribe {}
