67fd60ca6a0eea21afc092e04cb17e9adfe8203d
ukaushik-98 <kaushikuk101@gmail.com>
2025-01-08 23:27:16 -0800

    open server

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,32 @@
+use anyhow::Result;
 use clap::Parser;
+use tokio::{io::AsyncWriteExt, net::TcpListener};
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+    // number of replicas
+    #[arg(short, long, default_value_t = 2)]
+    replica_count: u8,
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    // master port
+    #[arg(short, long, default_value_t = 8999)]
+    port: u16,
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
+    let address = format!("127.0.0.1:{}", args.port);
+    let listener = TcpListener::bind(&address).await?;
+    println!("Listening on {}", address);
+    loop {
+        let (mut stream, _) = listener.accept().await?;
+        let _ = tokio::spawn(async move {
+            match stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").await {
+                Ok(_) => println!("Pusehd 200"),
+                Err(err) => panic!("{}", err),
+            }
+        });
     }
 }
